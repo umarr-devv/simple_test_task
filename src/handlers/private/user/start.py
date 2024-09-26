f031cea296d45c7b46179fd39f25792e6a5d3dff
umarr <umarr.developer@gmail.com>
2024-09-26 14:58:30 +0600

    create Username state group

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -4,6 +4,10 @@ from aiogram.filters import CommandStart, CommandObject
 router = Router()
 
 
+class Username(StatesGroup):
+    get_username = State()
+
+
 @router.message(CommandStart())
 async def on_start(message: types.Message, command: CommandObject):
     text = f'Добро пожаловать, {message.from_user.full_name}'
